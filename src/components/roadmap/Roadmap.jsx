8b6d937eabd0ae2b49c272697bb5753528c5d85d
Wes King <wes@boone.io>
2022-02-21 14:18:12 -0600

    Add Coming Soon to Roadmap.jsx

--- src/components/roadmap/Roadmap.jsx
+++ src/components/roadmap/Roadmap.jsx
@@ -3,7 +3,7 @@ import "./roadmap.scss"
 export default function Roadmap() {
     return(
         <div className="roadmap" id="roadmap">
-            <h2>Zoo Poker Roadmap</h2>
+            <h2>Zoo Poker Roadmap (Coming Soon)</h2>
         </div>
     )
 }
